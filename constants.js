7c6fb96ddf8177f65b92956084c4257b60757855
Junya Ishihara <junya@champierre.com>
2024-02-17 08:46:49 +0900

    初期座標を指定できるようにする。

--- constants.js
+++ constants.js
@@ -1,2 +1,5 @@
 const API_KEY = 'AIzaSyDuGsDZbA_qhW_994yBcxGTwh7QssOqGEw';
 const SHEET_ID = '14ND-oHmPE1Qu-mu4-VFnQ9zIuJQBrirUlg3UgbNZykI';
+const DEFAULT_LAT = 35.2335841;
+const DEFAULT_LNG = 138.7226297;
+const DEFAULT_ZOOM = 17;
