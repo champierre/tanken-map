b46cd7b1e06efe34a3c76e8f4cdef5a449ebf1dc
Junya Ishihara <junya@champierre.com>
2024-02-16 23:50:57 +0900

    modified:   constants.js

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
-const API_KEY = 'AIzaSyDfE5maG37wvHL9Kya4taQod_AZ9kM4UiA';
+const API_KEY = 'AIzaSyDuGsDZbA_qhW_994yBcxGTwh7QssOqGEw';
 const SHEET_ID = '14ND-oHmPE1Qu-mu4-VFnQ9zIuJQBrirUlg3UgbNZykI';
