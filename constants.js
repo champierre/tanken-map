1f6372d4bc4df4325975b4b892f839c4823ca8d0
Junya Ishihara <junya@champierre.com>
2024-02-17 09:18:14 +0900

    写真をタップすると大きく表示

--- constants.js
+++ constants.js
@@ -2,4 +2,4 @@ const API_KEY = 'AIzaSyDuGsDZbA_qhW_994yBcxGTwh7QssOqGEw';
 const SHEET_ID = '14ND-oHmPE1Qu-mu4-VFnQ9zIuJQBrirUlg3UgbNZykI';
 const DEFAULT_LAT = 35.2335841;
 const DEFAULT_LNG = 138.7226297;
-const DEFAULT_ZOOM = 17;
+const DEFAULT_ZOOM = 16;
